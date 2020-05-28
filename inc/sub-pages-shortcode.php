104eccbefac12cb80a9265af06fbe8c2a34b01a5
Ricard Torres <ricard.torres@netcentric.biz>
2020-05-28 09:02:21 +0200

    Update sub-pages-shortcode.php

--- inc/sub-pages-shortcode.php
+++ inc/sub-pages-shortcode.php
@@ -3,10 +3,7 @@
 function wiki_childpages() {
   global $post;
 
-  if ( is_page() && $post->post_parent )
-    $childpages = wp_list_pages( 'sort_column=menu_order&title_li=&child_of=' . $post->post_parent . '&echo=0' );
-  else
-    $childpages = wp_list_pages( 'sort_column=menu_order&title_li=&child_of=' . $post->ID . '&echo=0' );
+  $childpages = wp_list_pages( 'sort_column=menu_order&title_li=&child_of=' . $post->ID . '&echo=0' );
 
   if ( $childpages ) {
     $string = '<span class="h3 d-block">Sub pages</span>';
